c61b80d43cce6ecf404c48eb4d2731b9e4d2bb86
Augustus Flynn <huytung139@gmail.com>
2023-10-08 13:16:55 +0700

    get parsed tx

--- scripts/getTransactionReceipt.ts
+++ scripts/getTransactionReceipt.ts
@@ -1,21 +1,9 @@
-import axios from 'axios'
 import dotenv from "dotenv";
+import { WrapperConnection } from '../ReadApi/WrapperConnection';
 dotenv.config();
 
-async function getTransactionReceipt(transactionSignature: string) {
-  const response = await axios.post(process.env.RPC_URL as string, {
-    "jsonrpc": "2.0",
-    "id": 2,
-    "method": "getTransaction",
-    "params": [
-      transactionSignature,
-      "json"
-    ]
-  });
-
-  const data = await response.data;
-  console.log(JSON.stringify(data));
-}
-
-getTransactionReceipt('53wZLfGptKanot4yLGvcWQLRxS3JLFTccoozGzSEG1XVgtE1rNW7RLnRAQsuepxtwQFviLxZ479ZBNi2vXvkqodU');
-
+const wrappedConnection = new WrapperConnection(process.env.RPC_URL as string, 'confirmed');
+wrappedConnection.getParsedTransaction("53wZLfGptKanot4yLGvcWQLRxS3JLFTccoozGzSEG1XVgtE1rNW7RLnRAQsuepxtwQFviLxZ479ZBNi2vXvkqodU")
+  .then((res) => {
+    console.log(JSON.stringify(res))
+  })
